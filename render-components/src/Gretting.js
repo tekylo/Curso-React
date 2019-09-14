97ce7cef15fdcf55c8c984321cb01ba41639c5f7
React <react@ZiMac2-06.local>
2019-09-14 11:02:46 +0200

    Mejora ejercicio módulo 2 - 1

--- render-components/src/Gretting.js
+++ render-components/src/Gretting.js
@@ -1,5 +1,3 @@
 import React from 'react';
 
-const gretting = () => <h1> ¡Hola! Este es mi primer componente!</h1>
-
-export default gretting
+export default () => <h1> ¡Hola! Este es mi primer componente!</h1>
