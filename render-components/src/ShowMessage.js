6b15e3c4a1a3ae38150b2a914b7c67bd02d7e4fd
React <react@ZiMac2-06.local>
2019-09-14 11:34:26 +0200

    Mejora ejercicio modulo 2 - 4

--- render-components/src/ShowMessage.js
+++ render-components/src/ShowMessage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const ShowMessage = () => {
-    const user = {
-        msg: 'Ahora puedes leer esto',
-        flag: false
+    const message = {
+        text: 'Ahora puedes leer esto',
+        flag: true
     }
 
-    return  <p>{ user.flag ? user.msg : 'El mensaje esta desactivado' }</p>
+    return  <p>{ message.flag ? message.text : 'El mensaje esta desactivado' }</p>
 }
 
 export default ShowMessage
